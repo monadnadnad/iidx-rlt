7169aa8ec34c1e3031d71a35caff4e03d10cb145
monadnadnad <55864383+monadnadnad@users.noreply.github.com>
2025-12-01 01:26:34 +0900

    chore(style): ハーフモーダルのsafe area

--- src/components/ui/FloatingSheet.tsx
+++ src/components/ui/FloatingSheet.tsx
@@ -21,7 +21,6 @@ export const FloatingSheet: React.FC<FloatingSheetProps> = ({ open, onClose, tit
     <Box
       sx={{
         p: 2,
-        pb: `calc(${theme.spacing(2)} + env(safe-area-inset-bottom, 0px))`,
         display: "flex",
         flexDirection: "column",
         flex: 1,
@@ -54,6 +53,7 @@ export const FloatingSheet: React.FC<FloatingSheetProps> = ({ open, onClose, tit
               overflowY: "auto",
               borderTopLeftRadius: 12,
               borderTopRightRadius: 12,
+              pb: "env(safe-area-inset-bottom, 0px)",
               left: "env(safe-area-inset-left, 0px)",
               right: "env(safe-area-inset-right, 0px)",
               width: "auto",
